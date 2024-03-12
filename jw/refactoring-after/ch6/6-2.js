22833235a35e066ad574112acb7b183e320dd00c
우종원 <danielwoo@mathflat.com>
2024-03-12 23:51:17 +0900

    update: 6-3-1 예제

--- jw/refactoring-after/ch6/6-2.js
+++ jw/refactoring-after/ch6/6-2.js
@@ -1,20 +1,32 @@
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+  return moreThanFiveLateDeliveries(driver.numberOfLateDeliveries) ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+// numberOfLateDeliveries 값만 알면 되는 로직.
+// 따라서, driver 객체를 전달받지 않고, numberOfLateDeliveries 값만 전달받아도 충분하다.
+function moreThanFiveLateDeliveries(numberOfLateDeliveries) {
+  return numberOfLateDeliveries > 5;
 }
 
 // 예제 2
 function reportLines(customer) {
   const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
+
+  return gatherCustomerData(lines, customer);
 }
 
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
+// 지금은 코드가 잘 읽히고 예측이 용이하지만,
+// 복잡해졌을 때 이 함수가 어떤 동작을 하는지 파악하기 어려워질 수 있다.
+// => 결과 배열을 반환하는 순수 함수로 변경
+// 그리고 out이라는 이름은 너무 추상적이다. lines로 변경
+function gatherCustomerData(lines, customer) {
+  const resultLines = [...lines];
+
+  resultLines.push(["name", customer.name]);
+  resultLines.push(["location", customer.location]);
+
+  return resultLines;
 }
+
+console.log(reportLines({ name: "jw", location: "seoul" }));
--- jw/refactoring-after/ch6/6-3-1.js
+++ jw/refactoring-after/ch6/6-3-1.js
@@ -1,8 +1,90 @@
-export function price(order) {
-  // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+const getDeliveryPrice = (order) => {
+  return Math.min(order.quantity * order.itemPrice * 0.1, 100);
+};
+
+const getQuantityDiscount = (order) => {
+  return Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+};
+
+const getBasePrice = (order) => {
+  return order.quantity * order.itemPrice;
+};
+
+// FIXME: 함수 이름이 마음에 들지 않음. 함수의 목적을 더 잘 드러내는 이름으로 변경
+// price -> calculateTotalPrice
+export function calculateTotalPrice(order) {
+  const 기본가격 = getBasePrice(order);
+  const 수량할인 = getQuantityDiscount(order);
+  const 배송비 = getDeliveryPrice(order);
+
+  return 기본가격 - 수량할인 + 배송비; // 주석 없이도 이해하기 쉬워짐
 }
+
+// 객체는 이미 자신의 역할을 수행할 수 있는 데이터를 알고 있다. 모듈로 분리하면 더 좋을듯.
+// 함수로 하든 class로 하든 캡슐화 가능
+// 함수가 어디서 굴러다니지 않는다.
+// 함수 내부로직을 숨기고, 함수를 호출하는 코드에게 필요한 정보만 제공할 수 있다.
+const calculateTotalPriceModule = (order) => {
+  const getDeliveryPrice = () => {
+    return Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  };
+
+  const getQuantityDiscount = () => {
+    return Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  };
+
+  const getBasePrice = () => {
+    return order.quantity * order.itemPrice;
+  };
+
+  const calculateTotalPrice = () => {
+    const 기본가격 = getBasePrice();
+    const 수량할인 = getQuantityDiscount();
+    const 배송비 = getDeliveryPrice();
+
+    return 기본가격 - 수량할인 + 배송비;
+  };
+
+  return {
+    calculateTotalPrice,
+  };
+};
+
+class Order {
+  #quantity;
+  #price;
+
+  constructor(quantity, price) {
+    this.#quantity = quantity;
+    this.#price = price;
+  }
+
+  #getDeliveryPrice() {
+    return Math.min(this.#quantity * this.#price * 0.1, 100);
+  }
+
+  #getQuantityDiscount() {
+    return Math.max(0, this.#quantity - 500) * this.#price * 0.05;
+  }
+
+  #getBasePrice() {
+    return this.#quantity * this.#price;
+  }
+
+  calculateTotalPrice() {
+    const 기본가격 = this.#getBasePrice();
+    const 수량할인 = this.#getQuantityDiscount();
+    const 배송비 = this.#getDeliveryPrice();
+
+    return 기본가격 - 수량할인 + 배송비;
+  }
+}
+
+console.log(
+  calculateTotalPriceModule({
+    quantity: 600,
+    itemPrice: 1000,
+  }).calculateTotalPrice()
+);
+
+console.log(new Order(600, 1000).calculateTotalPrice());
